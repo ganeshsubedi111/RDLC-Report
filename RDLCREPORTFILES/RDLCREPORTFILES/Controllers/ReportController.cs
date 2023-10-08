1fd57cdb56a13e59d0b168e402f9615e1d700442
ganeshsubedi111 <subediganesh306@gmail.com>
2023-10-08 20:43:01 +0545

    Rdlc report using MVC web

---
+++ RDLCREPORTFILES/RDLCREPORTFILES/Controllers/ReportController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using AspNetCore.Reporting;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RDLCREPORTFILES.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ReportController(IWebHostEnvironment webHostEnvironment)
+        {
+            this._webHostEnvironment = webHostEnvironment;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Print()
+        {
+            string mimeType = "application/pdf";
+            int extension = 1;
+            var path = Path.Combine(this._webHostEnvironment.WebRootPath, "Reports", "Report1.rdlc");
+
+            Dictionary<string,string> parameters = new Dictionary<string, string>();
+            parameters.Add("rp", "Welcome to insoft");
+            LocalReport localReport = new LocalReport(path);
+            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimeType);
+
+            return File(result.MainStream, mimeType);
+        }
+    }
+}
